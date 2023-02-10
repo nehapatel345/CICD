9900be3ac14d96b259cd5544924dbb6cbf995c02
nehapatel345 <nehapatel345jbp@gmail.com>
2023-02-10 16:39:24 +0530

    work 5653

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ resource "google_compute_instance" "default" {
 
   tags = ["allow-firewall"]
 
+  allow_stopping_for_update = true
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
