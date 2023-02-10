80712e6c8acc555e6251f3436b2a703a1558b093
nehapatel345 <99860862+nehapatel345@users.noreply.github.com>
2023-02-10 15:01:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+
+resource "google_compute_instance" "default" {
+  name         = "test24"
+  machine_type = "e2-medium"
+  zone         = "us-central1-c"
+
+  tags = ["allow-firewall"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = "terraform-network"
+    subnetwork = "terraform-subnetwork-sub"
+
+  }
+
+}
